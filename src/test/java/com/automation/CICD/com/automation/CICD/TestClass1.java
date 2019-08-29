44f076ed16390f639be9163250b6aec77cb2031c
sameena.c.taz <sameena.c.taz@gmail.com>
2019-08-29 18:11:26 +0100

    first commit

---
+++ src/test/java/com/automation/CICD/com/automation/CICD/TestClass1.java
@@ -0,0 +1,46 @@
+package com.automation.CICD.com.automation.CICD;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+
+	public static WebDriver driver;
+	@BeforeMethod
+	public void launchdriver() {
+		System.out.println("Launching Browser");
+		System.setProperty("webdriver.chrome.driver", "C:\\BrowserDrivers\\chromedriver_win32\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		System.out.println("Launching Browser ----");
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+		
+	}
+	@Test
+	public void test1() {
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+	}
+	@Test
+	public void test2() throws InterruptedException {
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Java");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@name='btnK']")).click();
+		
+	}
+	@AfterMethod
+	public void close() {
+		driver.quit();
+		
+	}
+	
+}
